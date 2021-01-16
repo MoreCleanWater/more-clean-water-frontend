91855dc5b8e8955dcd9a90eba6883b0d06a288a8
Anisha James <73602224+anishajames@users.noreply.github.com>
2021-01-16 17:49:47 +0000

    clear text after save

--- src/components/Admin/AwarenessCategory/AwarenessCategory.jsx
+++ src/components/Admin/AwarenessCategory/AwarenessCategory.jsx
@@ -4,11 +4,9 @@ import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import { useState } from "react";
-import { DataGrid } from "@material-ui/data-grid";
+import AwarenessCategoryList from "./AwarenessCategoryList";
 import Snackbar from "@material-ui/core/Snackbar";
 import Alert from "@material-ui/lab/Alert";
-import DeleteIcon from "@material-ui/icons/Delete";
-import EditIcon from "@material-ui/icons/Edit";
 
 const useStyles = makeStyles(() => ({
   text: {
@@ -23,76 +21,31 @@ const useStyles = makeStyles(() => ({
       backgroundColor: "#A2EA2E",
     },
   },
-  //   header: {
-  //     "& .super-app-theme--header": {
-  //       backgroundColor: "#BEF8FB",
-  //       font: "bold",
-  //     },
-  //     height: "40%",
-  //     font: "bold",
-  //   },
 }));
 
 export default function AwarenessCategory() {
   const classes = useStyles();
-  const [disableButton, setDisableButton] = useState(true);
-  const [openMessage, setOpenMessage] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(false);
+  const [descText, setDescText] = useState("");
+  const [catText, setCatText] = useState("");
 
-  const handleChangeEvent = (event) => {
-    event.target.value !== null
-      ? setDisableButton(false)
-      : setDisableButton(true);
+  const handleSave = () => {
+    setSuccessMessage(true);
+    handleReset();
   };
 
-  const saveCategory = () => {
-    setOpenMessage(true);
+  const handleReset = () => {
+    setCatText("");
+    setDescText("");
+    // setDisableButton(true);
   };
 
   const handleClose = (reason) => {
     if (reason === "clickaway") {
       return;
     }
-    setOpenMessage(false);
+    setSuccessMessage(false);
   };
-
-  const columns = [
-    { field: "id", hide: true },
-    {
-      field: "category",
-      headerClassName: "super-app-theme--header",
-      headerName: <strong>Category</strong>,
-      flex: 0.5,
-    },
-    { field: "description", headerName: <strong>Description</strong>, flex: 1 },
-    {
-      field: "actions",
-      headerName: <strong>Actions</strong>,
-      flex: 0.25,
-      renderCell: () => (
-        <div>
-          <EditIcon style={{ cursor: "pointer", color: "green" }} />
-          <DeleteIcon style={{ cursor: "pointer", color: "red" }} />
-        </div>
-      ),
-    },
-  ];
-
-  const rows = [
-    {
-      id: 1,
-      category: "Preventive",
-      description: "Strategies to preventive spread of disease",
-    },
-    { id: 2, category: "Safety", description: "Safety warnings" },
-    { id: 3, category: "General", description: "" },
-    { id: 4, category: "Hygiene", description: "" },
-    {
-      id: 5,
-      category: "Equality",
-      description: "All are entitled to get water",
-    },
-    { id: 6, category: "Women and children", description: "" },
-  ];
 
   return (
     <div>
@@ -109,27 +62,34 @@ export default function AwarenessCategory() {
             required
             id="standard-basic-category"
             label="Category"
+            value={catText}
             className={classes.text}
-            onChange={handleChangeEvent}
+            onChange={(e) => {
+              setCatText(e.target.value);
+            }}
           />
           <TextField
             id="standard-basic-desc"
             label="Description"
+            value={descText}
             className={classes.text}
+            onChange={(e) => {
+              setDescText(e.target.value);
+            }}
           />
 
           <Button
             variant="contained"
-            disabled={disableButton}
+            disabled={catText === ""}
             size="large"
             className={classes.button}
-            onClick={saveCategory}
+            onClick={handleSave}
           >
             SAVE
           </Button>
           <Snackbar
-            open={openMessage}
-            autoHideDuration={6000}
+            open={successMessage}
+            autoHideDuration={4000}
             onClose={handleClose}
           >
             <Alert onClose={handleClose} severity="success">
@@ -137,9 +97,7 @@ export default function AwarenessCategory() {
             </Alert>
           </Snackbar>
         </Grid>
-        <Grid item xs={10} md={8} style={{ height: "60%", display: "flex" }}>
-          <DataGrid rows={rows} columns={columns} autoPageSize />
-        </Grid>
+        <AwarenessCategoryList />
       </Grid>
     </div>
   );
---
+++ src/components/Admin/AwarenessCategory/AwarenessCategoryList.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import Grid from "@material-ui/core/Grid";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+
+const columns = [
+  { field: "id", hide: true },
+  {
+    field: "category",
+    headerClassName: "super-app-theme--header",
+    headerName: <strong>Category</strong>,
+    flex: 0.5,
+  },
+  { field: "description", headerName: <strong>Description</strong>, flex: 1 },
+  {
+    field: "actions",
+    headerName: <strong>Actions</strong>,
+    flex: 0.25,
+    renderCell: () => (
+      <div>
+        <EditIcon style={{ cursor: "pointer", color: "green" }} />
+        <DeleteIcon style={{ cursor: "pointer", color: "red" }} />
+      </div>
+    ),
+  },
+];
+
+const rows = [
+  {
+    id: 1,
+    category: "Preventive",
+    description: "Strategies to preventive spread of disease",
+  },
+  { id: 2, category: "Safety", description: "Safety warnings" },
+  { id: 3, category: "General", description: "" },
+  { id: 4, category: "Hygiene", description: "" },
+  {
+    id: 5,
+    category: "Equality",
+    description: "All are entitled to get water",
+  },
+  { id: 6, category: "Women and children", description: "" },
+];
+
+export default function AwarenessCategoryList() {
+  return (
+    <Grid item xs={10} md={8} style={{ height: "60%", display: "flex" }}>
+      <DataGrid rows={rows} columns={columns} autoPageSize />
+    </Grid>
+  );
+}
